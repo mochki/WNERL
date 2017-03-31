5f30f3a3cd927372ddb747ac63770fd555b6992f
mochki <grapefruitnights@protonmail.com>
2017-03-31 12:11:17 -0600

    More

---
+++ node_edge_creation/part_11.py
@@ -0,0 +1,33 @@
+import ignore
+import pickle
+from peewee import *
+
+db = MySQLDatabase('Wikilinks', user='root', passwd=ignore.password_string, charset='utf8mb4')
+class LongTextField(TextField):
+    db_field = 'longtext'
+class Page(Model):
+    title = CharField()
+    links = LongTextField()
+
+    class Meta:
+        database = db
+db.connect()
+nodes = {}
+
+# 5000001-5538056
+
+for x in range(5000001, 5538057):
+    current_page = Page.select().where(Page.id == x).get()
+    current_links = current_page.links.split("###")
+
+    for node in current_links:
+        if node in nodes:
+            nodes[node] += 1
+        else:
+            nodes[node] = 1
+
+    if not x % 50000:
+        print(x)
+
+with open('nodes_11.pickle', 'wb') as handle:
+    pickle.dump(nodes, handle, protocol=pickle.HIGHEST_PROTOCOL)
