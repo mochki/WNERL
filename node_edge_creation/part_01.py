a4639c75db40a669d31131e9aeacafef793898c7
mochki <grapefruitnights@protonmail.com>
2017-03-31 12:06:41 -0600

    Node Creation!

--- node_edge_creation/part_01.py
+++ node_edge_creation/part_01.py
@@ -3,41 +3,21 @@ import pickle
 from peewee import *
 
 db = MySQLDatabase('Wikilinks', user='root', passwd=ignore.password_string, charset='utf8mb4')
-
 class LongTextField(TextField):
     db_field = 'longtext'
-
 class Page(Model):
     title = CharField()
     links = LongTextField()
 
     class Meta:
         database = db
-
 db.connect()
-# db.create_tables([Page])
-# AFTER creation, go to my sql work bench and change links to utf8mb4
-
 nodes = {}
-max_id = Page.select(fn.MAX(Page.id)).scalar()
-print(max_id)
-
 
 #       1- 500000
-#  500001-1000000
-# 1000001-1500000
-# 1500001-2000000
-# 2000001-2500000
-# 2500001-3000000
-# 3000001-3500000
-# 3500001-4000000
-# 4000001-4500000
-# 4500001-5000000
-# 5000001-5538056
 
-
-for x in range(max_id):
-    current_page = Page.select().where(Page.id == x+1).get()
+for x in range(1, 500001):
+    current_page = Page.select().where(Page.id == x).get()
     current_links = current_page.links.split("###")
 
     for node in current_links:
@@ -46,11 +26,8 @@ for x in range(max_id):
         else:
             nodes[node] = 1
 
-    if not x % 10000:
+    if not x % 50000:
         print(x)
 
 with open('nodes_01.pickle', 'wb') as handle:
     pickle.dump(nodes, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-
-# print(list(range(1,51)))
